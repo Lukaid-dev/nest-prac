2d1807ad5e42c11e812025680fb94b05407fa072
Seongwoo_Lukaid <seongwoo.lee@jjaann.com>
2023-04-26 16:23:34 +0900

    type orm start

--- src/apis/boards/entities/board.entities.ts
+++ src/apis/boards/entities/board.entities.ts
@@ -1,22 +1,23 @@
-// import { Field, Int, ObjectType } from '@nestjs/graphql';
-// import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-// @Entity() // for TypeORM, table
-// @ObjectType() // for GraphQL
-// export class Board {
-//   @PrimaryGeneratedColumn('increment')
-//   @Field(() => Int) // for GraphQL
-//   number: number;
+// entity에는 s안붙음
+@Entity() // for TypeORM, table
+@ObjectType() // for GraphQL
+export class Board {
+  @PrimaryGeneratedColumn('increment')
+  @Field(() => Int) // for GraphQL
+  number: number;
 
-//   @Column()
-//   @Field(() => String) // for GraphQL
-//   writer: string;
+  @Column()
+  @Field(() => String) // for GraphQL
+  writer: string;
 
-//   @Column()
-//   @Field(() => String) // for GraphQL
-//   title: string;
+  @Column()
+  @Field(() => String) // for GraphQL
+  title: string;
 
-//   @Column()
-//   @Field(() => String) // for GraphQL
-//   content: string;
-// }
+  @Column()
+  @Field(() => String) // for GraphQL
+  content: string;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './apis/boards/boards.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +10,17 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', // docker시에는 name resolution
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'myProject',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      // logging: true,
     }),
   ],
 })
